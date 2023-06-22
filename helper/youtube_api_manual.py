6fd84869ec2d89812f6c9c633e0d51a145bfce22
klepininalex <klepininalex@gmail.com>
2023-06-22 15:55:37 +0300

    homework 1

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -6,9 +6,8 @@ from googleapiclient.discovery import build
 
 import isodate
 
-
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.environ.get('API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
